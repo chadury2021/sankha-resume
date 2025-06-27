9940cead61e9b5480e933100b270b60962758757
miqart <miqart1@gmail.com>
2025-06-27 20:34:17 +0400

    Desktop and tablet versions of new design

---
+++ src/components/reusable/DateCard.tsx
@@ -0,0 +1,48 @@
+import { motion } from 'framer-motion';
+
+import InfoCard, { IInfoCardProps } from './InfoCard';
+import { cn } from '../../lib/utils.ts';
+
+export interface IDateCardProps {
+  date: string;
+  index: number;
+  items: IInfoCardProps[];
+  equalCols?: boolean;
+}
+
+const DateCard = ({ date, index, items, equalCols = false }: IDateCardProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: index * 0.1, duration: 0.5 }}
+      className={cn('grid grid-cols-1 gap-8', {
+        'md:grid-cols-[120px_auto]': !equalCols,
+        'md:grid-cols-2': equalCols,
+      })}
+    >
+      <div className="flex flex-col items-start">
+        <div className="px-2 py-1 bg-[#17A0BF1A] rounded-[4px] h-5 flex items-center whitespace-nowrap">
+          <div className="dateTagDot mr-2.5" />
+          <span className="font-shuriken text-[10px] tracking-[2%] text-[#17A0BF]">{date}</span>
+        </div>
+        <div className="ml-2.5 mt-2 bg-[#17A0BF1A] h-full w-[1px] max-md:hidden" />
+      </div>
+      <div className="flex flex-col gap-y-4">
+        {items.map(({ title, description, descriptionsList, Icon }, itemIndex) => (
+          <InfoCard
+            key={title}
+            title={title}
+            description={description}
+            descriptionsList={descriptionsList}
+            Icon={Icon}
+            type="info"
+            index={itemIndex}
+          />
+        ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default DateCard;
---
+++ src/pages/Education/Education.tsx
@@ -0,0 +1,58 @@
+import { motion } from 'framer-motion';
+
+import DateCard from '../../components/reusable/DateCard';
+
+const data = [
+  {
+    date: '2007-2012',
+    items: [
+      {
+        title: 'PhD in Applied Mathematics',
+        description: 'Massachusetts Institute of Technology',
+        Icon: <img src="/External/mit.png" alt="mit" />,
+      },
+      {
+        title: 'MS in Computer Science',
+        description: 'FAU, Germany',
+        Icon: <img src="/External/fau.png" alt="fau" />,
+      },
+    ],
+  },
+  {
+    date: '2007-2012',
+    items: [
+      {
+        title: 'BE in Computer Science',
+        description: 'TUM, Germany',
+        Icon: <img src="/External/tum.png" alt="tum" />,
+      },
+    ],
+  },
+];
+
+const Education = () => {
+  return (
+    <div className="flex flex-col justify-center h-full">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h1 className="section-title text-sm">Education</h1>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+        className="flex flex-col gap-y-8"
+      >
+        {data.map(({ date, items }, index) => (
+          <DateCard key={index} date={date} index={index} items={items} equalCols />
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Education;
